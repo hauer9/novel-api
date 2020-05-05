6681b7437bc532ed28fafce2caf6602012e31221
tuscany <dingzh@chinamobiad.com>
2020-05-05 21:00:15 +0800

    ^^

--- server.ts
+++ server.ts
@@ -25,6 +25,6 @@ const port = 8004;
 
   app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
-    console.log(`Press CTRL-C to stop ^^ \n`)
+    console.log(`Press CTRL-C to stop \n`)
   })
 })()
