3cd3bc3d3558ac1fb2d3556f5136047930f59574
tuscany <dingzh@chinamobiad.com>
2020-05-07 21:56:56 +0800

    test

--- server.ts
+++ server.ts
@@ -20,11 +20,11 @@ const port = process.env.PORT || 8080;
   })
 
   /* 
-   * Listen port   
+   * Listen port
    */
 
   app.listen(port, () => {
-    console.log(`Server is running at http://0.0.0.0:${port}`)
+    console.log(`Server is running at http://localhost:${port}`)
     console.log(`Press CTRL-C to stop \n`)
   })
 })()
