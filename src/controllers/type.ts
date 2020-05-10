9e04ed79e89d2103756c3764ccd3a893ff7e6a4f
tuscany <dingzh@chinamobiad.com>
2020-05-10 17:19:26 +0800

    change return the count of the get novels by types  function

--- src/controllers/type.ts
+++ src/controllers/type.ts
@@ -10,9 +10,14 @@ class Type extends BaseCtrl {
   async getNovelsByTypes(ctx: any) {
     const q = ctx.query
 
-    const data = await TypeModel.scope('novels').findAndCountAll(q)
-    
-    ctx.success(data)
+    const { rows, count: novelCount } = await TypeModel.scope('novels').findAndCountAll(q)
+    const { count } = await TypeModel.findAndCountAll(q)
+
+    ctx.success({
+      count,
+      novelCount,
+      rows,
+    })
   }
 }
 
